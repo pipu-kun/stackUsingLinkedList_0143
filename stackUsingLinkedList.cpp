6357260b9425c80426e40bb06a69969ebfa82d8f
pipu-kun <SideActivity@proton.me>
2026-05-14 10:39:15 +0700

    add function of push func

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -20,6 +20,13 @@ class stack{
             top = NULL; // awalnya top itu NULL
         }
 
-        int push(int value){}
+        int push(int value){
+            node *newNode = new node(); // buat node baru
+            newNode->data = value; // isi data node baru dengan value yang diberikan
+            newNode->next = top; // buat next node baru menunjuk ke top (node sebelumnya)
+            top = newNode; // update top untuk menunjuk ke node baru (sekarang node baru adalah top)
+            cout << "Push value: " << value << endl;    
+            return value;   
+        }
 };
 
